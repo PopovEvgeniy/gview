e525814342165d0d0e6dfc871349bee4c7bd2f6e
Evgeniy Popov <tuzik87@inbox.ru>
2017-05-02 00:10:33 +0600

    Last stable release. First public release on github

---
+++ gvcode.pas
@@ -0,0 +1,104 @@
+unit gvcode; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses Classes, Forms, Controls, Graphics, ShellCtrls, FileCtrl, ExtCtrls, ComCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    FileListBox1: TFileListBox;
+    Image1: TImage;
+    ShellTreeView1: TShellTreeView;
+    procedure FileListBox1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
+    procedure ShellTreeView1Change(Sender: TObject; Node: TTreeNode);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var Form1: TForm1;
+procedure window_setup();
+procedure viewer_setup();
+procedure load_image();
+procedure resize_window();
+procedure setup();
+
+implementation
+
+procedure window_setup();
+begin
+ Application.Title:='GRAPHIC VIEW';
+ Form1.Caption:='GRAPHIC VIEW 2.0.8';
+ Form1.Font.Name:=Screen.MenuFont.Name;
+ Form1.Font.Size:=14;
+end;
+
+procedure viewer_setup();
+begin
+Form1.Image1.Stretch:=False;
+Form1.Image1.Center:=False;
+Form1.Image1.Proportional:=True;
+Form1.Image1.AutoSize:=False;
+Form1.FileListBox1.Sorted:=True;
+Form1.FileListBox1.Mask:='*.bmp;*.jpg;*.ico;*.emf;*.wmf *.png';
+end;
+
+procedure load_image();
+begin
+if Form1.FileListBox1.FileName<>'' then
+begin
+Form1.Image1.Picture.LoadFromFile(Form1.FileListBox1.FileName);
+Form1.Image1.Canvas.AutoRedraw:=True;
+end;
+
+end;
+
+procedure resize_window();
+begin
+Form1.FileListBox1.Height:=Form1.ClientHeight-Form1.ShellTreeView1.Top-270;
+Form1.Image1.Width:=Form1.ClientWidth-Form1.ShellTreeView1.Left-235;
+Form1.Image1.Height:=Form1.ClientHeight-10;
+end;
+
+procedure setup();
+begin
+window_setup();
+viewer_setup();
+resize_window();
+end;
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+setup();
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+resize_window();
+end;
+
+procedure TForm1.FileListBox1Click(Sender: TObject);
+begin
+load_image();
+end;
+
+procedure TForm1.ShellTreeView1Change(Sender: TObject; Node: TTreeNode);
+begin
+FileListBox1.Directory:=ShellTreeView1.GetSelectedNodePath();
+end;
+
+{$R *.lfm}
+
+{ TForm1 }
+
+end.
---
+++ readme.txt
@@ -0,0 +1,54 @@
+                                Graphic view
+                                
+                                Version 2.0.8
+
+                        Created by Popov Evgeniy Alekseyevich
+
+Program description
+
+This program is a simple graphic viewer
+
+System requirement
+
+WINDOWS 98 or higher 
+
+Supported formats
+
+JPEG
+BITMAP
+ICON
+WINDOWS METAFILE
+PORTABLE NETWORK GRAPHICS
+
+License
+
+This program distributed under GNU GENERAL PUBLIC LICENSE 
+
+Contact
+
+You can send me a letter to tuzik87@inbox.ru
+
+About program modification
+
+Source code is included in source.zip archive. 
+It can be compiled under Lazarus 0.9.30 or higher.
+
+Version history
+
+1.0 — Initial version.
+
+1.1:
+Help converted to htm help format
+Totally program rewrote. Program license changed to GNU GENERAL PUBLIC LICENSE
+Program recompiled under BORLAND TURBO C++ 2006 EXPLORER
+Installer changed
+
+2.0 — Totally program rewrote.
+2.0.1 — System integration added.
+2.0.2 — Program improved
+2.0.3 — Program improved
+2.0.4 — Totally program rewrote.
+2.0.5 — Program improved
+2.0.6 - Small changes in source code.
+2.0.7 - Small bugs fixed.
+2.0.8 - Small changes in program
